39091af28650b086d078b16875c80bb48aed3705
mallabonikki <mallabonikki6678@gmail.com>
2017-02-12 17:27:51 +1100

    seconde commit

---
+++ RawInput/RawInputEventArg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RawInput_dll
+{
+    public class RawInputEventArg : EventArgs
+    {
+        public RawInputEventArg(KeyPressEvent arg)
+        {
+            KeyPressEvent = arg;
+        }
+        
+        public KeyPressEvent KeyPressEvent { get; private set; }
+
+    }
+}
